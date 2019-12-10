9d7aacc49fbf8f0cb237a65de064bb8497759cda
tesar-tech <jan@tesar.tech>
2019-12-10 18:11:03 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Editing;
+using Windows.Media.Effects;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ReachFramesVideoEffect;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReachFrames
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            //set file to process
+            StorageFile videoFile = await Package.Current.InstalledLocation.GetFileAsync("big_buck_bunny.mp4");
+
+            MediaClip clip = await MediaClip.CreateFromFileAsync(videoFile);
+
+            //applay "effect" on clip
+            var vefdef = new VideoEffectDefinition(typeof(VideoEffectReachFrames).FullName);
+            clip.VideoEffectDefinitions.Add(vefdef);
+
+            compositor = new MediaComposition();
+            compositor.Clips.Add(clip);
+        }
+
+
+        MediaComposition compositor;
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var tempFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("tempFile.mp4", CreationCollisionOption.ReplaceExisting);
+            //create temp file 
+
+            await compositor.RenderToFileAsync(tempFile);//render to file to envoke VideoEffect..
+        }
+    }
+}
---
+++ ReachFramesVideoEffect/VideoEffectReachFrames.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
+
+namespace ReachFramesVideoEffect
+{
+    public sealed class VideoEffectReachFrames: IBasicVideoEffect
+    {
+        public VideoEffectReachFrames()
+        {
+
+        }
+
+
+        public void ProcessFrame(ProcessVideoFrameContext context)
+        {
+            //Getingt pixel data
+
+            var inputFrameBitmap = context.InputFrame.SoftwareBitmap;
+            var frameSize = inputFrameBitmap.PixelWidth * inputFrameBitmap.PixelHeight * 4;
+
+            var frameBuffer = new Buffer((uint)frameSize);
+            context.InputFrame.SoftwareBitmap.CopyToBuffer(frameBuffer);
+            var framePixels = frameBuffer.ToArray();
+
+            Debug.WriteLine($"Touching 25th red pixel {framePixels[100]}");
+        }
+        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
+        {
+        }
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+
+        }
+
+        public void DiscardQueuedFrames()
+        {
+        }
+
+        public bool IsReadOnly { get; }
+        public IReadOnlyList<VideoEncodingProperties> SupportedEncodingProperties { get; } = new List<VideoEncodingProperties>();
+        public MediaMemoryTypes SupportedMemoryTypes { get; } = MediaMemoryTypes.Cpu;// MediaMemoryTypes.GpuAndCpu
+        public bool TimeIndependent { get; } = true;
+
+        public void SetProperties(IPropertySet configuration)
+        {
+        }
+    }
+}
